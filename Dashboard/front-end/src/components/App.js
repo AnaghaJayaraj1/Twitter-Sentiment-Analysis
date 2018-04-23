f4ef913ecabd360be39e270c73e9c1185283d234
panchaltej <tejaspanchal49@gmail.com>
2018-04-22 18:29:25 -0700

    Added graph to analyze result

--- Dashboard/front-end/src/components/App.js
+++ Dashboard/front-end/src/components/App.js
@@ -5,18 +5,35 @@ import {Bar, Line, Pie} from 'react-chartjs-2';
 class App extends Component {
 
     state = {
-        tweet_pred: []
+        tweet_pred: [],
+        positive_count: 0,
+        negative_count: 0,
+        tweet_pred_display: [],
+        isHold: false
     };
 
     constructor(props) {
         super(props);
         subscribeToQueue((err, tweet) => {
-            var tweets = this.state.tweet_pred;
-            console.log(tweet);
-            tweets.push(JSON.parse(tweet));   
-            this.setState({
-                tweet_pred : tweets
-        })});
+            if(!this.state.isHold){
+                var tweets = this.state.tweet_pred;
+                console.log(tweet);
+                tweet = JSON.parse(tweet);
+                tweets.push(tweet); 
+                if(tweet.sentiment == "-ve"){  
+                    this.setState({
+                        tweet_pred : tweets,
+                        negative_count : this.state.negative_count + 1
+                    })
+                }
+                else{
+                    this.setState({
+                        tweet_pred : tweets,
+                        positive_count : this.state.positive_count + 1
+                    })
+                }
+            }
+        });
     }
 
     createItemsList(){
@@ -59,12 +76,65 @@ class App extends Component {
                     </div>
                 </nav>
 
-                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+                <div class="col-md-9 ml-sm-auto col-lg-10">
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2">Dashboard</h1>
+                        <div class="btn-toolbar mb-2 mb-md-0">
+                            <div class="btn-group mr-2">
+                            {this.state.isHold ?
+                                <button class="btn btn-sm btn-outline-secondary"
+                                    onClick={(event) => 
+                                        this.setState({
+                                            isHold: false
+                                        })
+                                    }>
+                                Resume
+                                </button>:
+                                <button class="btn btn-sm btn-outline-secondary"
+                                    onClick={(event) => 
+                                        this.setState({
+                                            isHold: true
+                                        })
+                                    }>
+                                Pause
+                                </button>
+                            }
+                            </div>
+                        </div>
                     </div>
-
-                    <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+                    <div className = "row">
+                        <div  className = "col-md-3 ml-sm-3 col-lg-3"/>
+                        <div class="my-4 w-50" width="500px" height="180px" position="absolute">
+                            <Bar data={{
+                                    labels: ["positive", "negative"],
+                                    datasets: [
+                                        {
+                                            label:"Tweets",
+                                            data:[(this.state.positive_count)/(this.state.positive_count + this.state.negative_count) * 100, 
+                                                this.state.negative_count/(this.state.positive_count + this.state.negative_count) * 100],
+                                            backgroundColor:['rgba(124, 254, 0, 0.6)','rgba(255, 0, 0, 0.6)']
+                                        }
+                                        ]
+                                    }}
+                                options={{
+                                title:{
+                                display:"display", text:"Analysis Result", fontSize:15},
+                                legend:{
+                                display:"false", position:"xyz"},
+                                scales: {
+                                    yAxes: [{
+                                        display: true,
+                                        ticks: {
+                                            beginAtZero: true,
+                                            max: 100
+                                        }
+                                    }]
+                                },
+                                responsive: true,
+                                maintainAspectRatio: true}}/>
+                        </div>
+                        <div  className = "col-md-3 ml-sm-3 col-lg-3"/>
+                    </div>
 
                     <h2>Tweet Sentiments</h2>
                     <div class="table-responsive">
@@ -80,7 +150,7 @@ class App extends Component {
                             </tbody>
                         </table>
                     </div>
-                </main>
+                </div>
             </div>
         </div>
         );
